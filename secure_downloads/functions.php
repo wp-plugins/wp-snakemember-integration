72013e68f151b96e37840cc66f36c80c4f5d910a
snakemember <snakemember@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-05 12:53:20 +0000

    Tagged version 1.6, content filtering on loops & menus
    git-svn-id: https://plugins.svn.wordpress.org/wp-snakemember-integration/tags/1.6@1153765 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- secure_downloads/functions.php
+++ secure_downloads/functions.php
@@ -27,44 +27,52 @@ function wp_sm_secure_downloads_parse_request( &$wp )
 
 function wp_sm_get_secure_s3_url($hash, $as_attachment = false){
   $url = base64_decode($hash);
+  
+  if(strpos($url,'amazonaws.com') !== false){
+    // This is an Amazon S3 URL
+    
+    //$clean_url=$url;
+    $to_remove="/https?:\/\/s3.*?\.amazonaws.com\//";
+    $to_remove_normal_urls=array("/https?:\/\//", "/\.s3\.amazonaws\.com/");
 
-  //$clean_url=$url;
-  $to_remove="/https?:\/\/s3.*?\.amazonaws.com\//";
-  $to_remove_normal_urls=array("/https?:\/\//", "/\.s3\.amazonaws\.com/");
-
-  //quito subtring de la url
-  $clean_url = preg_replace($to_remove, "", $url);
-  //die(serialize($clean_url));
-  $clean_url = preg_replace($to_remove_normal_urls, "", $clean_url); 
+    //quito subtring de la url
+    $clean_url = preg_replace($to_remove, "", $url);
+    //die(serialize($clean_url));
+    $clean_url = preg_replace($to_remove_normal_urls, "", $clean_url); 
  
-  $accessKey = get_option('sm_s3_api_key');
-  $secretKey = get_option('sm_s3_api_secret');
+    $accessKey = get_option('sm_s3_api_key');
+    $secretKey = get_option('sm_s3_api_secret');
 
-  $expiry_time = get_option('sm_s3_expiry_time') ? get_option('sm_s3_expiry_time') : '+2 days';
+    $expiry_time = get_option('sm_s3_expiry_time') ? get_option('sm_s3_expiry_time') : '+2 days';
 
-  $timestamp = strtotime($expiry_time);
-  $strtosign = "GET\n\n\n$timestamp\n/$clean_url";
+    $timestamp = strtotime($expiry_time);
+    $strtosign = "GET\n\n\n$timestamp\n/$clean_url";
   
-  if($as_attachment){
-    $strtosign = $strtosign."?response-content-disposition=attachment";
-  }
+    if($as_attachment){
+      $strtosign = $strtosign."?response-content-disposition=attachment";
+    }
 
-  $signature = urlencode(base64_encode(hash_hmac("sha1", utf8_encode($strtosign), $secretKey, true)));
+    $signature = urlencode(base64_encode(hash_hmac("sha1", utf8_encode($strtosign), $secretKey, true)));
 
-  $destination_url = "$url?AWSAccessKeyId=$accessKey&Expires=$timestamp&Signature=$signature";
+    $destination_url = "$url?AWSAccessKeyId=$accessKey&Expires=$timestamp&Signature=$signature";
   
-  if($as_attachment){
-    $destination_url = $destination_url."&response-content-disposition=attachment";
+    if($as_attachment){
+      $destination_url = $destination_url."&response-content-disposition=attachment";
+    }
+  } else {
+    // This is an internal or "regular" URL
+    $destination_url = $url;
   }
   
   return $destination_url;
 }
 
 function wp_sm_secure_s3_link( $atts, $content = "" ) {
+
 	$atts = shortcode_atts( array(
 		'url' => '',
 		'css_class' => ''
-	), $atts, 'sm_s3_secure_download' );
+	), $atts, 's3_secure_download' );
 
   $base64_url = base64_encode($atts['url']);
   $css_class = $atts['css_class'];
@@ -87,7 +95,7 @@ function wp_sm_secure_s3_link( $atts, $content = "" ) {
 function wp_sm_secure_s3_video( $atts ) {
 	$atts = shortcode_atts( array(
 		'url' => ''
-	), $atts, 'sm_s3_secure_video' );
+	), $atts, 's3_secure_video' );
   
   $base64_url = base64_encode($atts['url']);
   
@@ -95,14 +103,4 @@ function wp_sm_secure_s3_video( $atts ) {
 
   return do_shortcode("[video src=\"$secure_url\" ]");
   
-}
-
-add_action('init', 'wp_sm_add_s3_shortcodes');
-
-function wp_sm_add_s3_shortcodes() {
-  remove_shortcode('sm_s3_secure_download');
-  remove_shortcode('sm_s3_secure_video');
-  
-  add_shortcode( 'sm_s3_secure_download', 'wp_sm_secure_s3_link' );
-  add_shortcode( 'sm_s3_secure_video', 'wp_sm_secure_s3_video' );
 }
