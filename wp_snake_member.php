8bdab21001d117aa57ed5b7417c6ee841b8d247d
snakemember <snakemember@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-26 16:20:05 +0000

    Tagged version 1.7, added OTO shorttags
    git-svn-id: https://plugins.svn.wordpress.org/wp-snakemember-integration/trunk@1167834 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp_snake_member.php
+++ wp_snake_member.php
@@ -5,7 +5,7 @@ Plugin URI: http://snakemember.com/
 Description: Wordpress integration plugin for SnakeMember
 Author: Michele Cumer
 Author URI: http://www.snakemember.com
-Version: 1.6.1
+Version: 1.7
 */
 
 // WP Activerecord
@@ -24,6 +24,9 @@ require_once realpath(dirname(__FILE__)) . "/area_adm/view.php";
 require_once realpath(dirname(__FILE__)) . "/area_adm/ajax.php";
 // Secure downloads
 require_once realpath(dirname(__FILE__)) . "/secure_downloads/functions.php";
+
+// OTOs
+require_once realpath(dirname(__FILE__)) . "/otos/functions.php";
 
 function sm_wp_activerecord_init(){
     ActiveRecord\Config::initialize(function($cfg)
